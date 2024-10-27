3968febf94aacc0f3d207bf0452ced970c3527e5
AYUSH KHAIRE <141166916+AYUSHKHAIRE@users.noreply.github.com>
2024-10-27 23:42:41 +0530

    Update settings.py

--- stocksly/settings.py
+++ stocksly/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECERT_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG","False").lower() == "true"
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(' ')
 DATABASE_URL = os.environ.get("DATABASE_URL")
